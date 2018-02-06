85c8561ab1aaf26fcf891fd80442ef179dc50ae3
Marcin Osajca <marcin9608@gmail.com>
2018-02-06 20:31:03 +0100

    add files generated by spring initializr

---
+++ src/test/java/book/catalogue/BookCatalogueApplicationTests.java
@@ -0,0 +1,16 @@
+package book.catalogue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookCatalogueApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
