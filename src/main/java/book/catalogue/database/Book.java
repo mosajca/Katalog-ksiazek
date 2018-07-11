ef7067faf374e43aa191ea32443e428f41cf7acb
Marcin Osajca <marcin9608@gmail.com>
2018-07-11 12:25:58 +0200

    set strategy for GeneratedValue

--- src/main/java/book/catalogue/database/Book.java
+++ src/main/java/book/catalogue/database/Book.java
@@ -6,6 +6,7 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -15,7 +16,7 @@ import javax.persistence.OneToMany;
 public class Book {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
