fdd9370c2428ec295fd87324ebe6b74c86554159
Marcin Osajca <marcin9608@gmail.com>
2018-08-17 19:57:55 +0200

    add GlobalExceptionHandler

---
+++ src/main/java/book/catalogue/controllers/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package book.catalogue.controllers;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DataAccessException.class)
+    public ResponseEntity<?> handle() {
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+}
